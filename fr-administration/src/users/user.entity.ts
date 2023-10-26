4a9f6159987cb587299a1c5765cac5d55a555a8c
@axemartin <axel.martin.1@etudiant.univ-rennes1.fr>
2023-10-26 12:45:50 +0200

    Changes by theov

--- fr-administration/src/users/user.entity.ts
+++ fr-administration/src/users/user.entity.ts
@@ -3,11 +3,13 @@ export class User {
     id: number;
     lastname: string;
     firstname: string;
+    age: number;
 
-    constructor(id:number,lastname: string, firstname: string) {
+    constructor(id:number,lastname: string, firstname: string, age:number) {
         this.lastname = lastname;
         this.firstname = firstname;
         this.id=id;
+        this.age=age
     }
 
 }
