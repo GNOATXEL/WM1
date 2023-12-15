8ee82bb5fa1a166706fba69e31dfb63bce75b854
@axemartin <axel.martin.1@etudiant.univ-rennes1.fr>
2023-12-15 11:13:44 +0100

    base_url

--- fr-administration/src/main.ts
+++ fr-administration/src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({ 'origin':'http://localhost:4200' });
   app.use(helmet());
 
   const config = new DocumentBuilder()
