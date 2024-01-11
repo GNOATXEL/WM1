a2a25482f55271ff14742f850c774ba2989c8b0b
@axemartin <axel.martin.1@etudiant.univ-rennes1.fr>
2024-01-11 22:07:47 +0100

    Maintenant c'est SMQ et on peut choisir les groupes.

--- fr-administration-front/src/app/idol/idol.component.ts
+++ fr-administration-front/src/app/idol/idol.component.ts
@@ -12,8 +12,9 @@ import {ApiHelperService} from "../../services/api-helper.service";
 
 export class IdolComponent {
 
-  constructor(private http: HttpClient,private router: Router,private api: ApiHelperService,
-              private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private http: HttpClient, private router: Router, private api: ApiHelperService,
+              private el: ElementRef, private renderer: Renderer2) {
+  }
 
   dataSource: any[] = [];
   dataSourcent: any[] = [];
@@ -21,7 +22,7 @@ export class IdolComponent {
   showSuggestions: boolean = false;
   inputValue: string = '';
   answerValue: string = '';
-  result: string='';
+  result: string = '';
   isStartClicked: boolean = false;
   isNextClicked: boolean = true;
   skippable: boolean = true;
@@ -31,28 +32,31 @@ export class IdolComponent {
   total: number = 0;
   skipped: boolean = false;
   rightAnswer: string = "";
-  group: string= "";
+  group: string = "";
+  status: number = 0;
+  songPool: any[] = [];
   YT: any;
   @ViewChild('inputField') inputField!: ElementRef;
 
   ngOnInit(): void {
     const request: Observable<any> = this.http.get(
       'http://localhost:3000/song',
-      { observe: 'response' },
+      {observe: 'response'},
     );
     request.subscribe({
-      next: (response) => (this.dataSourcent = response.body,console.log(this.dataSourcent)),
+      next: (response) => (this.dataSourcent = response.body, console.log(this.dataSourcent)),
     });
 
   }
+
   start(): void {
-    this.starting=true;
-    this.isStartClicked=true;
-    this.isNextClicked=true;
-    this.skippable=false;
+    this.starting = true;
+    this.isStartClicked = true;
+    this.isNextClicked = true;
+    this.skippable = false;
     const request: Observable<any> = this.http.put(
-      'http://localhost:3000/song/roll',
-      { observe: 'response' },
+      'http://localhost:3000/song/roll/' + this.status,
+      {observe: 'response'},
     );
 
     request.subscribe({
@@ -69,65 +73,70 @@ export class IdolComponent {
 
         const videoFrame = document.getElementById('videoFrame') as HTMLVideoElement;
         videoFrame.src = newVideoUrl;
-        let seconds= 20;
+        let seconds = 20;
         videoFrame.classList.add('hidden');
 
-        seconds+=2;
+        seconds += 2;
 
         const countdownTimer = document.getElementById('countdownTimer') as HTMLParagraphElement;
         countdownTimer.style.display = 'block';
         countdownTimer.textContent = `Loading...`;
-        let self=this;
-        videoFrame.onload = function (){
-        countdownTimer.textContent = `Loaded!`;
-        self.inputField.nativeElement.focus();
-        self.skipped=false;
-        const timerInterval = setInterval(() => {
-          seconds--;
-          self.shiftenter=true;
-          if (seconds !==0 ){countdownTimer.textContent = `${seconds-1}`;}
-          if (seconds === 0 || self.skipped) {
+        let self = this;
+        videoFrame.onload = function () {
+          countdownTimer.textContent = `Loaded!`;
+          self.inputField.nativeElement.focus();
+          self.skipped = false;
+          const timerInterval = setInterval(() => {
+            seconds--;
+            self.shiftenter = true;
+            if (seconds !== 0) {
+              countdownTimer.textContent = `${seconds - 1}`;
+            }
+            if (seconds === 0 || self.skipped) {
 
-            setTimeout(() => {clearInterval(timerInterval);
-              countdownTimer.textContent = ``;
-              videoFrame.classList.remove('hidden');
-              countdownTimer.style.display = 'none';
+              setTimeout(() => {
+                clearInterval(timerInterval);
+                countdownTimer.textContent = ``;
+                videoFrame.classList.remove('hidden');
+                countdownTimer.style.display = 'none';
 
-              self.isNextClicked=false;
-              self.skippable=true;
-              self.rightAnswer=self.dataSource[0].name;
-              self.group=self.dataSource[0].group.trim();
-              if(self.answerValue.toLowerCase() === self.dataSource[0].name.toLowerCase().replace(/・/g, ' ') ||
-                self.inputValue.toLowerCase() === self.dataSource[0].name.toLowerCase().replace(/・/g, ' ')){
-                self.result="trouvé";
-                self.guesses+=1;
-                self.total+=1;
-                self.shiftenter=true;
-              }else{
-                self.result="raté";
-                self.total+=1;
-                self.shiftenter=true;
-              }
-              },100);
+                self.isNextClicked = false;
+                self.skippable = true;
+                self.rightAnswer = self.dataSource[0].name;
+                self.group = self.dataSource[0].group.trim();
+                if (self.answerValue.toLowerCase() === self.dataSource[0].name.toLowerCase().replace(/・/g, ' ') ||
+                  self.inputValue.toLowerCase() === self.dataSource[0].name.toLowerCase().replace(/・/g, ' ')) {
+                  self.result = "correct";
+                  self.guesses += 1;
+                  self.total += 1;
+                  self.shiftenter = true;
+                } else {
+                  self.result = "wrong";
+                  self.total += 1;
+                  self.shiftenter = true;
+                }
+              }, 100);
 
-          }; // Actualise toutes les secondes
-        },1000)};
-        this.result="";
-        this.answerValue="";
+            }
+            ; // Actualise toutes les secondes
+          }, 1000)
+        };
+        this.result = "";
+        this.answerValue = "";
         this.inputValue = '';
-        this.starting=false;
+        this.starting = false;
 
 
       }
     });
   }
 
-  answer():void{
-    this.answerValue=this.inputValue;
+  answer(): void {
+    this.answerValue = this.inputValue;
 
   }
 
-  skip(): void{
+  skip(): void {
     this.skipped = true;
   }
 
@@ -140,6 +149,7 @@ export class IdolComponent {
     console.log(this.dataSourcent);
   }
 
+
   writeSuggestion(suggestion: string) {
     this.inputValue = suggestion;
     this.showSuggestions = false;
@@ -151,12 +161,12 @@ export class IdolComponent {
   handleKeyboardEvent(event: KeyboardEvent): void {
     // Vérifier si la touche appuyée est "Enter" et si la touche Shift est également enfoncée
     if (event.key === 'Enter' && event.shiftKey && !this.skipped && this.shiftenter) {
-      this.shiftenter=false;
+      this.shiftenter = false;
       const button = this.el.nativeElement.querySelector('#skip');
       if (button) {
         button.click();
       }
-     }
+    }
     // else if (event.key === 'Enter' && event.shiftKey && !this.starting && this.shiftenter) {
     //    this.shiftenter=false;
     //   const button = this.el.nativeElement.querySelector('#next');
@@ -167,7 +177,50 @@ export class IdolComponent {
     // casse des trucs jsp pk à mon avis c trop lent
   }
 
+  activeButton: number = 0; // Initialisation à zéro
 
 
+  updateStatus(nb: number): void {
 
+    const nogi = document.getElementById('nogi') as HTMLButtonElement ;
+    const saku = document.getElementById('saku') as HTMLButtonElement ;
+    const hina = document.getElementById('hina') as HTMLButtonElement ;
+    const keya = document.getElementById('keya') as HTMLButtonElement ;
+    if (nb === 1) {
+      if (this.status % 10 === 0) {
+        this.status += nb
+        nogi.classList.add('active');
+
+      } else if (this.status % 10 === 1) {
+        this.status -= nb
+        nogi.classList.remove('active');
+      }
+    } else if (nb === 10) {
+      if (Math.floor(this.status / nb) % 10 === 0) {
+        this.status += nb
+        saku.classList.add('actives');
+      } else if (Math.floor(this.status / nb) % 10) {
+        this.status -= nb
+        saku.classList.remove('actives');
+      }
+    } else if (nb === 100) {
+      if (Math.floor(this.status / nb) % 10 === 0) {
+        this.status += nb
+        hina.classList.add('active');
+      } else if (Math.floor(this.status / nb) % 10) {
+        this.status -= nb
+        hina.classList.remove('active');
+      }
+    } else if (nb === 1000) {
+      if (Math.floor(this.status / nb) % 10 === 0) {
+        this.status += nb
+        keya.classList.add('active');
+      } else if (Math.floor(this.status / nb) % 10) {
+        this.status -= nb
+        keya.classList.remove('active');
+      }
+    }
+    console.log(this.status)
+
+  }
 }
--- fr-administration/src/song/song.controller.ts
+++ fr-administration/src/song/song.controller.ts
@@ -13,6 +13,11 @@ export class SongController {
     constructor(private service: SongService) {
     }
 
+    private nogi = 0;
+    private saku = 0;
+    private hina = 0;
+    private keya = 0;
+
     @Post()
     @ApiCreatedResponse({
         description: 'The song has been successfully created.'
@@ -50,27 +55,57 @@ export class SongController {
     @ApiCreatedResponse({
         description: 'Song has been successfully returned'
     })
-    async load(@Param() parameter,@Body() input: any): Promise<void> {
+    async load(@Param() parameter, @Body() input: any): Promise<void> {
         this.service.clear();
-        let k=0;
+        let k = 0;
         fs.readFile(input.filePath, 'utf-8', (err, data) => {
             if (err) {
                 console.error('Erreur de lecture du fichier :', err);
                 return;
             }
             const lines: string[] = data.split('\n');
-            lines.forEach((line: string) => {
-                const dataArray: string[] = line.split('|');
-                //console.log(dataArray);
-                if (dataArray.length > 0) {
-                    dataArray[0] = dataArray[0].replace('watch?v=', 'embed/');
+            const delay = 3; // délai en millisecondes
+
+            const addDataWithDelay = async (dataArray: string[]) => {
+                return new Promise<void>((resolve) => {
+                    setTimeout(() => {
+                        if (dataArray.length > 0) {
+                            dataArray[0] = dataArray[0].replace('watch?v=', 'embed/');
+                        }
+                        if (dataArray.length > 0) {
+                            dataArray[3] = dataArray[3].replace(/\r/g, '');
+                        }
+
+                        if(dataArray[4].trim()==="Nogizaka46"){
+                            this.nogi+=1;
+                        } else if(dataArray[4].trim()==="Sakurazaka46"){
+                            this.saku+=1;
+                        } else if(dataArray[4].trim()==="Hinatazaka46"){
+                            this.hina+=1;
+                        } else if(dataArray[4].trim()==="Keyakizaka46"){
+                            this.keya+=1;
+                        }
+                        console.log(this.nogi+' '+this.hina+' '+this.saku+' '+this.keya);
+
+                        this.service.create(dataArray[0], parseInt(dataArray[1]), parseInt(dataArray[2]), dataArray[3], dataArray[4]);
+                        resolve();
+                    }, delay);
+                });
+
+
+            };
+
+            const processLines = async () => {
+                for (const line of lines) {
+                    const dataArray: string[] = line.split('|');
+                    console.log(dataArray);
+                    await addDataWithDelay(dataArray);
                 }
-                if (dataArray.length > 0) {
-                    dataArray[3] = dataArray[3].replace(/\r/g, '');
-                }
-                this.service.create(dataArray[0], parseInt(dataArray[1]), parseInt(dataArray[2]), dataArray[3], dataArray[4]);
-            });
+            };
+
+            processLines();
         });
+
     }
 
 
@@ -82,8 +117,168 @@ export class SongController {
         let r = Math.floor(Math.random() *
             (await this.service.getMaxId() - await this.service.getSmallestId()+1) + await this.service.getSmallestId())
          const a = await this.service.getById(r);
-        console.log(a);
+        console.log("roll" + a);
         return a;
+    }
+
+    @Put('roll/:arg')
+    @ApiCreatedResponse({
+        description: 'Song has been successfully returned'
+    })
+    async Roll2(@Param() parameter): Promise<Song> {
+        let minId: number, maxId: number,minId2: number, maxId2: number, weight: number, weight2: number;
+
+        switch (parseInt(parameter.arg)) {
+            case 1:
+                minId = await this.service.getSmallestId();
+                maxId = minId + this.nogi - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("nogi");
+                break;
+            case 10:
+                minId = await this.service.getSmallestId() + this.nogi;
+                maxId = minId + this.saku - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("saku");
+                break;
+            case 11:
+                minId = await this.service.getSmallestId();
+                maxId = minId + this.nogi + this.saku- 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("nogi + saku");
+                break;
+            case 100:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku;
+                maxId = minId + this.hina - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("hina");
+                break;
+            case 101:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku;
+                maxId = minId + this.hina - 1;
+                minId2 = await this.service.getSmallestId();
+                maxId2 = minId2 + this.nogi - 1;
+                weight = this.hina/(this.hina+this.nogi);
+                weight2 = this.nogi/(this.hina+this.nogi)
+                console.log("hina + nogi");
+                break;
+
+            case 110:
+                minId = await this.service.getSmallestId() + this.nogi;
+                maxId = minId + this.saku + this.hina - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("hina + saku");
+                break;
+
+            case 111:
+                minId = await this.service.getSmallestId();
+                maxId = minId + this.nogi + this.saku + this.hina - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("hina + saku + nogi");
+                break;
+
+            case 1000:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku + this.hina;
+                maxId = minId + this.keya - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("keya");
+                break;
+
+            case 1001:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku + this.hina;
+                maxId = minId + this.keya - 1;
+                minId2 = await this.service.getSmallestId();
+                maxId2 = minId2 + this.nogi - 1;
+                weight = this.keya/(this.keya+this.nogi);
+                weight2 = this.nogi/(this.keya+this.nogi)
+                console.log("keya + nogi");
+                break;
+
+            case 1010:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku + this.hina;
+                maxId = minId + this.keya - 1;
+                minId2 = await this.service.getSmallestId() + this.nogi;
+                maxId2 = minId2 + this.saku - 1;
+                weight = this.keya/(this.keya+this.saku);
+                weight2 = this.saku/(this.keya+this.saku)
+                console.log("keya + saku");
+                break;
+
+            case 1011:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku + this.hina;
+                maxId = minId + this.keya - 1;
+                minId2 = await this.service.getSmallestId();
+                maxId2 = minId2 + this.nogi + this.saku - 1;
+                weight = this.keya/(this.keya+this.saku+this.nogi);
+                weight2 = (this.saku+this.nogi)/(this.keya+this.saku+this.nogi)
+                console.log("keya + saku + nogi");
+                break;
+
+            case 1100:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku;
+                maxId = minId +this.hina + this.keya - 1;
+                minId2 = 0;
+                maxId2 = 0;
+                weight = 1;
+                weight2 = 0
+                console.log("keya + hina");
+                break;
+
+            case 1101:
+                minId = await this.service.getSmallestId() + this.nogi + this.saku;
+                maxId = minId + this.hina + this.keya - 1;
+                minId2 = await this.service.getSmallestId();
+                maxId2 = minId2 + this.nogi - 1;
+                weight = (this.keya+this.hina)/(this.keya+this.hina+this.nogi);
+                weight2 = (this.nogi)/(this.keya+this.hina+this.nogi)
+                console.log("keya + hina + nogi");
+                break;
+
+            default:
+                minId = await this.service.getSmallestId();
+                maxId = await this.service.getMaxId();
+                minId2 = 0
+                maxId2 = 0
+                weight = 1
+                weight2 = 0
+                console.log("mouais");
+                break;
+        }
+
+        let r = Math.floor(Math.random() * (maxId - minId + 1) + minId);
+        let r2 = Math.floor(Math.random() * (maxId2 - minId2 + 1) + minId2);
+        let r3 = Math.random();
+        if(r3<=weight){
+            const a = await this.service.getById(r);
+            console.log(r3+" "+ a + r+ " r "+ minId + " " + maxId);
+            return a;
+        } else{
+            const a = await this.service.getById(r2);
+            console.log(r3+" "+ a + r2+ " r2 "+ minId2 + " " + maxId2);
+            return a;
+        }
+
     }
 
 }
